21fd2294bd8ecfb83a26fc3435bfc8a14787597b
Kevin Sui <suistem8@gmail.com>
2017-12-30 22:18:08 -0500

    Initial commit

---
+++ ListApp2/app/src/main/java/com/example/listapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.listapp;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdaptor = new ItemAdapter(this, items, prices, descriptions);
+        myListView.setAdapter(itemAdaptor);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent showPicture = new Intent(getApplicationContext(), DetailActivity.class);
+                showPicture.putExtra("example.com.ITEM_INDEX", i);
+                startActivity(showPicture);
+            }
+        });
+
+    }
+}
